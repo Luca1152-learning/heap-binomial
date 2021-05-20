245802506a5717c93d7a557b2af44429f6195f73
Luca1152 <t.luca1152@gmail.com>
2021-05-20 13:25:11 +0300

    Add input parsing
    Now, the (problem's) input is parsed from the input file. I tested this code to make sure that the input won't be a bottleneck.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+int n, q, op, m, x, a, b;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    freopen("mergeheap.in", "r", stdin);
+    freopen("mergeheap.out", "w", stdout);
+
+    // Input rapid
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> q;
+
+    for (int i = 0; i < q; i++) {
+        cin >> op;
+        if (op == 1) {
+            cin >> m >> x;
+        } else if (op == 2) {
+            cin >> m;
+        } else if (op == 3) {
+            cin >> a >> b;
+        }
+    }
+
     return 0;
 }
